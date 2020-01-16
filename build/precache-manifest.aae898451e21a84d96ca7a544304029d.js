6406b265a0ec7ca9bba4d64e162cf9797f101064
unknown <Martin Rene Larios@dev-mlarios.oc.ld.co.cr>
2020-01-15 23:09:00 -0600

    Initial commit

---
+++ build/precache-manifest.aae898451e21a84d96ca7a544304029d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43995f6f6970d60f8aecabeb3e5f331",
+    "url": "/monster-rodolex/index.html"
+  },
+  {
+    "revision": "1c6ef0384c09ce9fe562",
+    "url": "/monster-rodolex/static/css/main.526cc1c2.chunk.css"
+  },
+  {
+    "revision": "303aeaf85072886ce98b",
+    "url": "/monster-rodolex/static/js/2.126c5025.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rodolex/static/js/2.126c5025.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c6ef0384c09ce9fe562",
+    "url": "/monster-rodolex/static/js/main.b38b656b.chunk.js"
+  },
+  {
+    "revision": "91ced473254b56c4dd34",
+    "url": "/monster-rodolex/static/js/runtime-main.3135ea88.js"
+  }
+]);
